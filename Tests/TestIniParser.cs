0aa76600bccbcda721c2cd9ced8281e93329a6c3
letrungkien211 <letrungkien211@gmail.com>
2015-02-07 22:16:20 +0900

    Add unit tests for read method

--- Tests/TestIniParser.cs
+++ Tests/TestIniParser.cs
@@ -10,14 +10,29 @@ namespace Tests
         [TestMethod]
         public void TestIniParserInitializeFromString()
         {
+            var resourcePath = @"..\..\Resources\TestIniParser.txt";
+            // Test initialize methods
             IniParser ini = new IniParser();
             ini.Initialize(Properties.Resources.TestIniParser);
+            AssertEqual(ref ini);
 
+            // Test constructor
+            ini = new IniParser(resourcePath);
+            AssertEqual(ref ini);
+
+            // Test text reader
+            ini = new IniParser();
+            ini.Read(resourcePath);
+            AssertEqual(ref ini);
+        }
+
+        public void AssertEqual(ref IniParser ini)
+        {
             Assert.AreEqual(ini.GetSetting("Abc", "a"), "b");
             Assert.AreEqual(ini.GetSetting("Def", "a"), "d");
             Assert.AreEqual(ini.GetSetting("Abc", "c"), "d");
             Assert.AreEqual(ini.GetSetting("Def", "c"), "d");
-
+            Assert.AreEqual(ini.GetSetting("xxx", "yyy"), null);
         }
     }
 }
